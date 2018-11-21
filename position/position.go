f94282affd7faae4d336b5dd9aba534a0a241faa
rkj <romainknezevic@gmail.com>
2018-11-21 10:33:11 +0100

    add position package, for papertrading

---
+++ position/position.go
@@ -0,0 +1,158 @@
+package position
+
+import (
+	"fmt"
+	"time"
+)
+
+type Direction bool
+
+const (
+	Short = Direction(false)
+	Long  = Direction(true)
+	Sell  = Short
+	Buy   = Long
+
+	DefaultFees = 0.0015
+)
+
+type State string
+
+const (
+	New    = State("new")
+	Active = State("active")
+	Closed = State("closed")
+)
+
+type Transaction struct {
+	Id        int
+	Time      time.Time
+	Direction Direction
+	Quantity  float64
+	Price     float64
+}
+
+func (t Transaction) Cost() float64 {
+	return t.Price * t.Quantity
+}
+
+type Position struct {
+	Id          int
+	State       State
+	Base, Quote string
+	Direction   Direction
+
+	FeesRate  float64
+	TotalFees float64
+	Total     float64
+	Traded    float64
+	AvgEntry  float64
+	AvgExit   float64
+	OpenTime  time.Time
+	CloseTime time.Time
+
+	Transactions []Transaction
+}
+
+func NewPosition(t time.Time, base, quote string, direction Direction) *Position {
+	return &Position{
+		Base: base, Quote: quote, Direction: direction, FeesRate: DefaultFees, OpenTime: t,
+	}
+}
+
+func (p Position) String() string {
+	format := "%.2f"
+	if p.AvgEntry < 1e-2 {
+		format = "%.1e"
+	}
+
+	return fmt.Sprintf("  %s @ "+format+" -> %s @ "+format+": %10.2f",
+		p.OpenTime.Format("2006-01-02 15:04 -0700"), p.AvgEntry,
+		p.CloseTime.Format("2006-01-02 15:04 -0700"), p.AvgExit,
+		p.Net())
+}
+
+func (p Position) Active() bool {
+	return p.State == Active
+}
+
+func (p Position) Cost() float64 {
+	return p.Total * p.AvgEntry
+}
+
+func (p Position) Net() float64 {
+	if p.AvgEntry == 0 || p.AvgExit == 0 {
+		return 0
+	}
+	net := p.Traded*p.AvgExit - p.Traded*p.AvgEntry
+	net -= p.TotalFees
+	if p.Direction == Long {
+		return net
+	} else {
+		return -net
+	}
+}
+
+func (p *Position) NetOnClose(pr float64) float64 {
+	pp := *p
+	pp.PaperClose(pr)
+	return pp.Net()
+}
+
+func (p *Position) AddTransaction(t Transaction) {
+	p.Transactions = append(p.Transactions, t)
+	if p.Direction == t.Direction {
+		p.AvgEntry = ((p.AvgEntry * p.Total) + (t.Price * t.Quantity)) /
+			(p.Total + t.Quantity)
+		p.Total += t.Quantity
+		p.State = Active
+	} else {
+		p.AvgExit = ((p.AvgExit * p.Traded) + (t.Price * t.Quantity)) /
+			(p.Traded + t.Quantity)
+		p.Traded += t.Quantity
+		if p.Traded >= p.Total {
+			p.State = Closed
+			p.CloseTime = t.Time
+		}
+	}
+}
+
+func (p *Position) PaperBuy(q, pr float64) {
+	p.PaperBuyAt(q, pr, time.Now())
+}
+
+func (p *Position) PaperBuyAt(q, pr float64, t time.Time) {
+	p.TotalFees += p.FeesRate * (q * pr)
+	p.AddTransaction(Transaction{
+		Direction: Buy,
+		Quantity:  q,
+		Price:     pr,
+		Time:      t,
+	})
+}
+
+func (p *Position) PaperSell(q, pr float64) {
+	p.PaperSellAt(q, pr, time.Now())
+}
+
+func (p *Position) PaperSellAt(q, pr float64, t time.Time) {
+	p.TotalFees += p.FeesRate * (q * pr)
+	p.AddTransaction(Transaction{
+		Direction: Sell,
+		Quantity:  q,
+		Price:     pr,
+		Time:      t,
+	})
+}
+
+func (p *Position) PaperClose(pr float64) {
+	p.PaperCloseAt(pr, time.Now())
+}
+
+func (p *Position) PaperCloseAt(pr float64, t time.Time) {
+	fn := p.PaperBuyAt
+	if p.Direction == Long {
+		fn = p.PaperSellAt
+	}
+	fn(p.Total-p.Traded, pr, t)
+}
---
+++ position/position_test.go
@@ -0,0 +1,83 @@
+package position
+
+import "testing"
+
+func TestTransaction_Cost(t *testing.T) {
+	tx := Transaction{
+		Price: 500, Quantity: 2,
+	}
+	if tx.Cost() != 1000 {
+		t.Errorf("expecting cost 1000 got %f", tx.Cost())
+	}
+}
+
+func TestPosition_Net(t *testing.T) {
+	p := Position{
+		Total:    10,
+		Traded:   5,
+		AvgEntry: 100,
+		AvgExit:  500,
+		FeesRate: 0,
+	}
+	p.Direction = Long
+	if p.Net() != 2000 {
+		t.Errorf("expecting 2000 gain for long position, got %f", p.Net())
+	}
+	p.Direction = Short
+	if p.Net() != -2000 {
+		t.Errorf("expecting 2000 gain for long position, got %f", p.Net())
+	}
+}
+
+func TestPosition_AddTransaction(t *testing.T) {
+	p := Position{
+		Direction: Long,
+		State:     Active,
+		FeesRate:  DefaultFees,
+	}
+
+	p.PaperBuy(1, 4)
+	if p.AvgEntry != 4 {
+		t.Errorf("p.AvgEntry != 4")
+	}
+	p.PaperBuy(1, 2)
+	if p.AvgEntry != 3 {
+		t.Errorf("p.AvgEntry != 3")
+	}
+
+	p.PaperSell(1, 10)
+	if p.AvgExit != 10 {
+		t.Errorf("p.AvgEntry != 3")
+	}
+	if p.State == Closed {
+		t.Errorf("position shouldn't be closed")
+	}
+	if p.Traded != 1 {
+		t.Errorf("position should've traded 1")
+	}
+
+	p.PaperBuy(2, 5)
+	if p.AvgEntry != 4 {
+		t.Errorf("p.AvgEntry != 4")
+	}
+	if p.Total != 4 {
+		t.Errorf("p.Total != 4")
+	}
+
+	p.PaperSell(3, 10)
+	if p.State != Closed {
+		t.Errorf("position should be closed")
+	}
+	if p.Traded != p.Total {
+		t.Errorf("traded != total")
+	}
+	if p.Net() != 6*4-(4*p.AvgEntry*p.FeesRate+4*p.AvgExit*p.FeesRate) {
+		t.Errorf("unexpected net worth, got %f", p.Net())
+	}
+}
+
+func TestPosition_PaperClose(t *testing.T) {
+	p := Position{Direction: Long}
+	p.PaperBuy(10, 10)
+	p.PaperClose(10)
+}
